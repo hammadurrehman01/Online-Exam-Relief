ffca294f171c5f7ac010bbc3f5a257d230586ace
hammadurrehman01 <hammadurrehman@techlabs.cc>
2024-12-25 16:17:02 +0500

    console removed

--- src/app/[...page]/page.tsx
+++ src/app/[...page]/page.tsx
@@ -541,9 +541,6 @@ export default async function Page(props: PageProps) {
     },
   ];
 
-  console.log("pathname", pathname);
-  
-
   const content = await builder
     .get("homepage", {
       apiKey: "15a1f6006b8b43d9a1f6953c09e3b979",
